7b6b96dd820165bf6ffdd7c362a2132e68ac0e4c
Chris <chris.ruegger@gmail.com>
2019-01-13 11:54:52 -0500

    added postgres sequence

--- postgres/init/readings.sql
+++ postgres/init/readings.sql
@@ -1,6 +1,6 @@
 
 create table readings (
-  id bigint not null primary key,
+  id serial not null primary key,
   sensor_id 	varchar(40),
   notes 	varchar(40),
   reading_time	timestamp with time zone default (current_timestamp at time zone 'EST'),
@@ -8,41 +8,41 @@ create table readings (
 );
 
 insert into readings
-(id, sensor_id, notes, reading_time, reading)
+(sensor_id, notes, reading_time, reading)
 values 
-(1000, 'sensor1', 'temperature', TIMESTAMP '2019-01-06 08:36:38', 72.11);
+('sensor1', 'temperature', TIMESTAMP '2019-01-06 08:36:38', 72.11);
 
 insert into readings
-(id, sensor_id, notes, reading_time, reading)
+(sensor_id, notes, reading_time, reading)
 values 
-(1001, 'sensor1', 'temperature', TIMESTAMP '2019-01-06 09:36:38', 74.00);
+('sensor1', 'temperature', TIMESTAMP '2019-01-06 09:36:38', 74.00);
 
 insert into readings
-(id, sensor_id, notes, reading_time, reading)
+(sensor_id, notes, reading_time, reading)
 values 
-(1002, 'sensor1', 'temperature', TIMESTAMP '2019-01-06 10:36:38', 81.66);
+('sensor1', 'temperature', TIMESTAMP '2019-01-06 10:36:38', 81.66);
 
 insert into readings
-(id, sensor_id, notes, reading_time, reading)
+(sensor_id, notes, reading_time, reading)
 values 
-(1003, 'sensor1', 'temperature', TIMESTAMP '2019-01-06 11:36:38', 82.89);
+('sensor1', 'temperature', TIMESTAMP '2019-01-06 11:36:38', 82.89);
 
 insert into readings
-(id, sensor_id, notes, reading_time, reading)
+(sensor_id, notes, reading_time, reading)
 values 
-(1004, 'sensor1', 'temperature', TIMESTAMP '2019-01-06 12:36:38', 80.12);
+('sensor1', 'temperature', TIMESTAMP '2019-01-06 12:36:38', 80.12);
 
 insert into readings
-(id, sensor_id, notes, reading_time, reading)
+(sensor_id, notes, reading_time, reading)
 values 
-(1005, 'sensor1', 'temperature', TIMESTAMP '2019-01-06 15:36:38', 85.21);
+('sensor1', 'temperature', TIMESTAMP '2019-01-06 15:36:38', 85.21);
 
 insert into readings
-(id, sensor_id, notes, reading_time, reading)
+(sensor_id, notes, reading_time, reading)
 values 
-(1006, 'sensor1', 'temperature', TIMESTAMP '2019-01-06 16:36:38', 90.33);
+('sensor1', 'temperature', TIMESTAMP '2019-01-06 16:36:38', 90.33);
 
 insert into readings
-(id, sensor_id, notes, reading_time, reading)
+(sensor_id, notes, reading_time, reading)
 values 
-(1007, 'sensor1', 'temperature', TIMESTAMP '2019-01-06 18:36:38', 92.54);
+('sensor1', 'temperature', TIMESTAMP '2019-01-06 18:36:38', 92.54);
