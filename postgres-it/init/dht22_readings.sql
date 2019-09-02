07c9cab2a56aaddb44e848e88746381290fc3cb1
Chris <chris.ruegger@gmail.com>
2019-09-02 16:56:57 -0400

    mods

--- postgres-it/init/dht22_readings.sql
+++ postgres-it/init/dht22_readings.sql
@@ -11,4 +11,15 @@ create table dht22_readings (
 insert into dht22_readings
 (sensor_id, notes, reading_time, temperature, humidity)
 values 
-('sensor3', 'on patio', TIMESTAMP '2019-01-13 15:36:38', 35.11, 88.50);
+('sensor1', 'living room', TIMESTAMP '2019-09-02 15:36:38', 77.11, 88.50);
+
+insert into dht22_readings
+(sensor_id, notes, reading_time, temperature, humidity)
+values 
+('sensor2', 'dining room', TIMESTAMP '2019-10-12 15:36:38', 68.44, 93.51);
+
+insert into dht22_readings
+(sensor_id, notes, reading_time, temperature, humidity)
+values 
+('sensor3', 'on patio', TIMESTAMP '2019-01-13 15:36:38', 35.11, 67.02);
+
